3605a024fdb1fea865b42aa3be0ea17176088d40
nirzaf <mfmfazrin1986@gmail.com>
2021-12-25 14:44:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Diagnostics.Runtime;
+using System;
+using System.Linq;
+
+namespace memleak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pid = int.Parse(args[0]);
+
+            while (true)
+            {
+                EnumRegions(pid);
+
+                GC.Collect();
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+        private static void EnumRegions(int pid)
+        {
+            using var target = DataTarget.CreateSnapshotAndAttach(pid);
+
+            foreach (var clrVersion in target.ClrVersions)
+            {
+                var runtime = clrVersion.CreateRuntime();
+
+                TouchOtherRegions(runtime);
+            }
+        }
+
+        private static void TouchOtherRegions(ClrRuntime runtime)
+        {
+            var heap = runtime.Heap;
+            heap.EnumerateRoots().Count();
+        }
+    }
+}
